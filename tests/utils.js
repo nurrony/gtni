9cb962226ce2910cb46634e90b0e86bec7a17c5a
Nur Rony <pro.nmrony@gmail.com>
2016-03-24 20:31:21 +0600

    added test for https url

--- tests/utils.js
+++ tests/utils.js
@@ -1,14 +1,27 @@
 import tape from 'tape';
 import utils from '../libs/utils'
 
-tape('should get corrent repo name', (test) => {
-  const repoURL = 'git@github.com:nmrony/gtni.git'
-  const input = utils.getRepoName(repoURL)
-  const output = 'gtni'
-  
-  test.equal(input, output)
-  test.end();
+tape('should get corrent repo name', (nested) => {
+  nested.test('from ssh url', (t1) => {
+    const repoURLSSH = 'git@github.com:nmrony/gtni.git'
+    const output = utils.getRepoName(repoURLSSH)
+    const result = 'gtni'
+    t1.plan(1)
+    t1.equal(output, result);
+  })
+
+  nested.test('from HTTPS url', (t1) => {
+    const repoURLHTTPS = 'https://github.com/nmrony/gtni.git'
+    const output = utils.getRepoName(repoURLHTTPS)
+    const result = 'gtni'
+    t1.plan(1)
+    t1.equal(output, result);
+  })
+
+  nested.end();
 })
+
+
 
 
 
