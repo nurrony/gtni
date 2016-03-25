f6e710418bd119b8519980e122be74106fd17a9b
Nur Rony <pro.nmrony@gmail.com>
2016-03-25 20:39:43 +0600

    new test case added

--- tests/utils.js
+++ tests/utils.js
@@ -3,22 +3,39 @@ import utils from '../libs/utils'
 
 tape('should get corrent repo name', (nested) => {
   nested.test('from ssh url', (t1) => {
-    const repoURLSSH = 'git@github.com:nmrony/gtni.git'
-    const output = utils.getRepoName(repoURLSSH)
-    const result = 'gtni'
+    const input = 'git@github.com:nmrony/gtni.git'
+    const result = utils.getRepoName(input)
+    const expection = 'gtni'
     t1.plan(1)
-    t1.equal(output, result);
+    t1.equal(result, expection);
   })
 
   nested.test('from HTTPS url', (t1) => {
-    const repoURLHTTPS = 'https://github.com/nmrony/gtni.git'
-    const output = utils.getRepoName(repoURLHTTPS)
-    const result = 'gtni'
+    const input = 'https://github.com/nmrony/gtni.git'
+    const result = utils.getRepoName(input)
+    const expection = 'gtni'
     t1.plan(1)
-    t1.equal(output, result);
+    t1.equal(result, expection);
   })
 
   nested.end();
+})
+
+tape('prepareArguments return git options', (test) => {
+  const input = {
+    h: true,
+    help: 'true',
+    all: true,
+    b: 'test-branch',
+    branch: 'test-branch',
+    u: true,
+    '$0': ['test']
+  }
+  const result = utils.prepareArguments(input);
+  const expection = '--all -u'
+
+  test.equal(result, expection);
+  test.end();
 })
 
 
