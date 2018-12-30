37e8070816147cfba361f150ba2d44c9899b68ff
Nur Rony <pro.nmrony@gmail.com>
2018-12-30 18:15:16 +0600

    build: switch from yarn to npm

--- src/npm/index.js
+++ src/npm/index.js
@@ -3,5 +3,5 @@ import shell from 'shelljs'
 export default function npmInstall (yarn = false, debug = '', done) {
   const env = process.env.NODE_ENV ? 'NODE_ENV=' + process.env.NODE_ENV + ' ' : ''
   const command = env + (yarn ? 'yarn install ' : 'npm i --no-spin --no-progress ') + debug
-  shell.exec(command, {silent: true, async: true}, done)
+  shell.exec(command, { silent: true, async: true }, done)
 }
